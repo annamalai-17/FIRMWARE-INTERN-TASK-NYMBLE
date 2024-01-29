29702dc807d13603430a55750a88af387b1805b9
annamalai-17 <157974769+annamalai-17@users.noreply.github.com>
2024-01-29 11:26:18 +0530

    Update nymble_pc_code.py

--- nymble_pc_code.py
+++ nymble_pc_code.py
@@ -1,21 +1,19 @@
 import serial
 import time
 
-port = 'COM10'  # port number
-baud_rate = 2400
 
+port = 'COM10'  # port number
+
+baud_rate = 2400 # baud rate 
+
+# Creating a serial connection
 ser = serial.Serial(port, baud_rate, timeout=1)
 
 try:
     file_path = r"C:\Users\lokke\Desktop\nymble_firmware_intern_annamalai\nymble_text.txt"
-
-    # Variables for measuring data transmission speed
-    bit_counter = 0
-    time_interval = 5  # Setting the time interval for measuring in seconds
-    start_time = time.time()
-
     while True:
-        user_input = input("Enter a character to send to Arduino (type 'exit' to quit): ")
+        # Getting user input for the file path
+        user_input = input("Enter any character to start the process (type 'exit' to quit): ")
         if user_input.lower() == 'exit':
             break
 
@@ -24,32 +22,26 @@ try:
             with open(file_path, 'r') as file:
                 file_content = file.read()
 
+            # Recording the start time for measuring data transmission speed
+            start_time = time.time()
+
             # Sending the file content to Arduino
             ser.write(file_content.encode())
 
             # Waiting for a moment to allow Arduino to process and send data back
             time.sleep(3)
-
-            # Recording the start time for measuring data reception speed
-            start_time = time.time()
-
+            
             # Reading and printing the looped-back data from Arduino
             received_data = ser.readline().decode('utf-8').rstrip()
+
+            elapsed_time = time.time() - start_time
             print("Received data from Arduino:", received_data)
 
-            # Updating counter
-            bit_counter += len(received_data) * 8
-
-            # Checking if the time interval has passed
-            elapsed_time = time.time() - start_time
-            if elapsed_time >= time_interval:
-                # Calculating and printing the bit rate
-                bit_rate = bit_counter / elapsed_time
-                print(f"Bit Rate: {bit_rate} bits/second")
-
-                # Reseting counters for the next interval
-                bit_counter = 0
-                start_time = time.time()
+            # Calculating and printing the real-time data transmission speed
+           
+            total_bits_transmitted = len(file_content) * 8
+            data_rate = total_bits_transmitted / elapsed_time
+            print(f"Data Transmission Speed: {data_rate} bits/second")
 
         except FileNotFoundError:
             print("File not found. Please enter a valid file path.")
